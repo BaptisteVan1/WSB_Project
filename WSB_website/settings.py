ca50e123801d21af863859e20ebb21ebe34fd6fb
BaptisteVan <37420271+BaptisteVan1@users.noreply.github.com>
2020-05-16 11:14:15 +0200

    Update settings.py

--- WSB_website/settings.py
+++ WSB_website/settings.py
@@ -131,16 +131,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
-<<<<<<< HEAD
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-<<<<<<< HEAD
-DATABASES['default'].update(db_from_env)
-=======
-DATABASES['default'].update(db_from_env)
->>>>>>> 960f9a1... This is a test commit with deployment preparations
-=======
->>>>>>> 8450091... Small update
