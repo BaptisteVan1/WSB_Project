ebfc5f9e3ae659321836d13a1dfa6417c3c305f4
BaptisteVan <37420271+BaptisteVan1@users.noreply.github.com>
2020-05-16 11:30:07 +0200

    Update wsgi.py

--- WSB_website/wsgi.py
+++ WSB_website/wsgi.py
@@ -14,13 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WSB_website.settings')
 
 application = get_wsgi_application()
-<<<<<<< HEAD
-
+import os
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-<<<<<<< HEAD
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-=======
-application = DjangoWhiteNoise(application)
->>>>>>> 960f9a1... This is a test commit with deployment preparations
-=======
->>>>>>> 8450091... Small update
