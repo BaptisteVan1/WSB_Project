71b46adda70c9e4898fff84cbf5791987dfadb2c
BaptisteVan <37420271+BaptisteVan1@users.noreply.github.com>
2020-05-17 10:20:16 +0200

    Update settings.py

--- WSB_website/settings.py
+++ WSB_website/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/restaurants/static/"
+STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
